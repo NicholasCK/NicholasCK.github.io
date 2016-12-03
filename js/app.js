11d77a3c23b372ce21b96335171d894a8dc4827f
Nicholas K <nckoretski@gmail.com>
2016-12-02 20:04:16 -0500

    clean js up

--- js/app.js
+++ js/app.js
@@ -1,103 +1,84 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-				// anchors: ['landingSec', 'bioSec', 'portfolioSec', 'contactSec'],
-				// // sectionsColor: ['#DAC500', '#1BBC9B', '#7E8F7C'],
-				// menu: '#menu',
-				// navigation: false,
-				// navigationPosition: 'right',
-				// navigationTooltips: ['Home', 'About', 'Portfolio' , 'Contact'],
-				responsiveWidth: 1100,
-                // responsiveHeight:750,
-            });
-
-	//   STATUS BARS
-  $('#showExamples').click(function(e){
-		e.stopPropagation();
-		e.preventDefault();
-		$('#examplesList').toggle();
-	});
-	
-	$('html').click(function(){
-		$('#examplesList').hide();
-	});
-  
-  $('.percentage-bar').each(function(){
-		$(this).find('.bar').animate({
-			width: $(this).attr('data-percent')
-		}, 6000);
-	});
+$('#fullpage').fullpage({
+			// anchors: ['landingSec', 'bioSec', 'portfolioSec', 'contactSec'],
+			// // sectionsColor: ['#DAC500', '#1BBC9B', '#7E8F7C'],
+			// menu: '#menu',
+			// navigation: false,
+			// navigationPosition: 'right',
+			// navigationTooltips: ['Home', 'About', 'Portfolio' , 'Contact'],
+			responsiveWidth: 1100,
+            // responsiveHeight:750,
+        });
 });
 
 // STATUS BARS IN PORTFOLIO SECTION //
 
-// $(document).ready(function() {	
-// 	console.log('');
+$(document).ready(function() {	
+$('#showExamples').click(function(e){
+	e.stopPropagation();
+	e.preventDefault();
+	$('#examplesList').toggle();
+});
 
-// 	$('#showExamples').click(function(e){
-// 		e.stopPropagation();
-// 		e.preventDefault();
-// 		$('#examplesList').toggle();
-// 	});
-	
-// 	$('html').click(function(){
-// 		$('#examplesList').hide();
-// 	});
-// });
+$('html').click(function(){
+	$('#examplesList').hide();
+});
+});
 
-// $(document).ready(function() {
-// 	$('.percentage-bar').each(function(){
-// 		$(this).find('.bar').animate({
-// 			width: $(this).attr('data-percent')
-// 		}, 6000);
-// 	});
-// });
+$(document).ready(function() {
+$('.percentage-bar').each(function(){
+	$(this).find('.bar').animate({
+		width: $(this).attr('data-percent')
+	}, 6000);
+});
+});
 
 
 // INTERACTIVE NAV MENU//
 
 (function() {
 
-	"use strict";
+"use strict";
 
-	var toggles = document.querySelectorAll(".c-hamburger");
+var toggles = document.querySelectorAll(".c-hamburger");
 
-	for (var i = toggles.length - 1; i >= 0; i--) {
-		var toggle = toggles[i];
-		toggleHandler(toggle);
-	};
+for (var i = toggles.length - 1; i >= 0; i--) {
+	var toggle = toggles[i];
+	toggleHandler(toggle);
+};
 
-	function toggleHandler(toggle) {
-		toggle.addEventListener( "click", function(e) {
-			e.preventDefault();
-			(this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-		}
-		);
+function toggleHandler(toggle) {
+	toggle.addEventListener( "click", function(e) {
+		e.preventDefault();
+		(this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+	}
+	);
+}
+
+var menuOpen = false;
+
+$( "#button" ).click(function() {
+	if ( menuOpen ) {
+		$( "#sidebarWrapper" ).hide( "fast" );	
+	} else {
+		$( "#sidebarWrapper" ).show( "fast" );	
 	}
 
-	var menuOpen = false;
+	menuOpen = !menuOpen;
+} );
 
-	$( "#button" ).click(function() {
-		if ( menuOpen ) {
-			$( "#sidebarWrapper" ).hide( "fast" );	
-		} else {
-			$( "#sidebarWrapper" ).show( "fast" );	
-		}
+$( "#sidebarWrapper" ).click(function() {
+	if ( menuOpen ) {
 
-		menuOpen = !menuOpen;
-	} );
+		$.fn.fullpage.setAutoScrolling(false)
+		$( "#sidebarWrapper" ).hide( "fast" );	
+	} else {
+		$.fn.fullpage.setAutoScrolling(false)
+		$( "#sidebarWrapper" ).show( "fast" );	
+	}
 
-	$( "#sidebarWrapper" ).click(function() {
-		if ( menuOpen ) {
-
-			$.fn.fullpage.setAutoScrolling(false)
-			$( "#sidebarWrapper" ).hide( "fast" );	
-		} else {
-			$.fn.fullpage.setAutoScrolling(false)
-			$( "#sidebarWrapper" ).show( "fast" );	
-		}
-
-		menuOpen = !menuOpen;
-	} );
+	menuOpen = !menuOpen;
+} );
 
 
 
@@ -108,44 +89,10 @@ $(document).ready(function() {
 
 jQuery(window).on('load',function() {
 
-	jQuery("#status").delay(300).fadeOut("slow");   
+jQuery("#status").delay(300).fadeOut("slow");   
 
-	jQuery("#preloader").delay(300).fadeOut("slow");
+jQuery("#preloader").delay(300).fadeOut("slow");
 
 });
-
-
-
-var isPhoneDevice = "ontouchstart" in document.documentElement; 
-$(document).ready(function() {
-	if(isPhoneDevice){
-            //mobile
-
-        }
-        else{
-
-
-                //desktop               
-                $.fn.fullpage();
-            }
-        });
-
-
-function touchStartHandler(event){
-	event.preventDefault();
-	var e = event.originalEvent;
-
-            //stopping the auto scroll to adjust to a section
-            if(options.fitToSection){
-            	$htmlBody.stop();
-            }
-
-            if(isReallyTouch(e)){
-            	var touchEvents = getEventsPage(e);
-            	touchStartY = touchEvents.y;
-            	touchStartX = touchEvents.x;
-            }
-        }
-
 
 
